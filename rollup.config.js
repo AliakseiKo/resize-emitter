3569ac177601da9bae0acc61ce6ea6b3446eb050
Aliaksei <AliakseiKo@gmail.com>
2020-05-16 06:15:56 +0300

    add not minified build file

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,18 @@ import { terser } from 'rollup-plugin-terser';
 export default [
   {
     input: 'src/ResizeEmitter.js',
-    output: {
-      file: 'lib/ResizeEmitter.js',
-      format: 'umd',
-      name: 'ResizeEmitter',
-      // sourcemap: true,
-      plugins: [terser()]
-    }
+    output: [
+      {
+        file: 'lib/ResizeEmitter.js',
+        format: 'umd',
+        name: 'ResizeEmitter'
+      },
+      {
+        file: 'lib/ResizeEmitter.min.js',
+        format: 'umd',
+        name: 'ResizeEmitter',
+        plugins: [terser()]
+      }
+    ]
   }
 ];
