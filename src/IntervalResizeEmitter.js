ff6ea9ff944e67dbbefd54ecb6834cf803c8afa9
Aliaksei <AliakseiKo@gmail.com>
2020-05-16 05:33:59 +0300

    improve performance IntervalResizeEmitter

--- src/IntervalResizeEmitter.js
+++ src/IntervalResizeEmitter.js
@@ -6,29 +6,53 @@ import {
 
 import { getSize, changedSize } from './utils';
 
+var checkingElements = [];
+var requestID = 0;
+var isLooping = false;
+
+function frameLoop() {
+  for (var i = 0; i < checkingElements.length; i++) {
+    if (changedSize(checkingElements[i].__resizeSensor__)) {
+      _dispatchEvent(checkingElements[i], 'resize');
+    }
+  }
+  requestID = _requestAnimationFrame(frameLoop);
+}
+
+function runFrameLoop() {
+  if (isLooping) return;
+  requestID = _requestAnimationFrame(frameLoop);
+  isLooping = true;
+}
+
+function stopFrameLoop() {
+  if (!isLooping) return;
+  _cancelAnimationFrame(requestID);
+  isLooping = false;
+}
+
 var IntervalResizeEmitter = {
   add: function (element) {
-    var sensor = {
+    if (element.__resizeSensor__) return;
+
+    element.__resizeSensor__ = {
       target: element,
       prevSize: getSize(element),
       requestID: 0
     };
 
-    element.__resizeSensor__ = sensor;
+    checkingElements.push(element);
 
-    function loop() {
-      if (changedSize(sensor)) _dispatchEvent(sensor.target, 'resize');
-      sensor.requestID = _requestAnimationFrame(loop);
-    }
-
-    sensor.requestID = _requestAnimationFrame(loop);
+    runFrameLoop();
   },
 
   remove: function (element) {
-    if (element.__resizeSensor__) {
-      _cancelAnimationFrame(element.__resizeSensor__.requestID);
-      delete element.__resizeSensor__;
-    }
+    if (!element.__resizeSensor__) return;
+
+    checkingElements.splice(checkingElements.indexOf(element), 1);
+    delete element.__resizeSensor__;
+
+    if (checkingElements.length === 0) stopFrameLoop();
   }
 };
 
